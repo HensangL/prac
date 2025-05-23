f80a6dbe0ec7b45c64dd279dd930b7e63c8a6fac
HensangL <lamahensang100@gmail.com>
2025-05-23 23:15:09 +0545

    ...

--- frontend/src/Qualification.jsx
+++ frontend/src/Qualification.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 
 function Qualification() {
   return (
-    <div>Qualification</div>
+    <div>
+      
+      <div className='flex '>
+        <div>
+        <h1 className='text-center font-medium text-yellow-300'>Technical skills</h1>
+        <ul>
+          <li>React.js</li>
+
+<li>Tailwind CSS</li>
+
+<li>JavaScript (ES6+)
+</li>
+<li>HTML5 & CSS3
+</li>
+<li>React Router DOM
+</li>
+<li>.NET & ASP.NET Core
+</li>
+<li>C# Programming
+</li>
+<li>Git & GitHub
+</li>        </ul>
+
+        </div>
+        <div><h1></h1>
+        <ul>
+          <li></li>
+        </ul>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
