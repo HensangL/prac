6d14800153c846b8f14924a5b6c44c0b61bb23a8
HensangL <lamahensang100@gmail.com>
2025-05-23 23:20:51 +0545

    ..

--- frontend/src/Qualification.jsx
+++ frontend/src/Qualification.jsx
@@ -1,40 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 function Qualification() {
   return (
-    <div>
-      
-      <div className='flex '>
+    <div className="p-6 bg-gray-900 min-h-screen">
+      <div className="flex flex-col md:flex-row gap-12">
         <div>
-        <h1 className='text-center font-medium text-yellow-300'>Technical skills</h1>
-        <ul>
-          <li>React.js</li>
-
-<li>Tailwind CSS</li>
-
-<li>JavaScript (ES6+)
-</li>
-<li>HTML5 & CSS3
-</li>
-<li>React Router DOM
-</li>
-<li>.NET & ASP.NET Core
-</li>
-<li>C# Programming
-</li>
-<li>Git & GitHub
-</li>        </ul>
-
+          <h1 className="text-yellow-400 text-3xl font-semibold mb-4">Technical skills</h1>
+          <ul className="ml-4 list-disc text-white space-y-2">
+            <li>React.js</li>
+            <li>Tailwind CSS</li>
+            <li>JavaScript (ES6+)</li>
+            <li>HTML5 & CSS3</li>
+            <li>React Router DOM</li>
+            <li>.NET & ASP.NET Core</li>
+            <li>C# Programming</li>
+            <li>Git & GitHub</li>
+          </ul>
         </div>
-        <div><h1></h1>
-        <ul>
-          <li></li>
-        </ul>
+        <div>
+          <h1 className="text-yellow-400 text-3xl font-semibold mb-4">Soft skills</h1>
+          <ul className="ml-4 list-disc text-white space-y-2">
+            <li>Problem-solving</li>
+            <li>Team Collaboration</li>
+            <li>Quick Learning</li>
+            <li>Communication</li>
+          </ul>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
 
-export default Qualification
+export default Qualification;
